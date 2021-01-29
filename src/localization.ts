570526b805f0c48dd2d8d733675919dd2f949be9
Liam Cain <hi@liamca.in>
2021-01-28 21:33:46 -0500

    Linting

--- src/localization.ts
+++ src/localization.ts
@@ -1,18 +1,20 @@
 import type { App } from 'obsidian';
 import { configureGlobalMomentLocale } from 'obsidian-calendar-ui';
-
 import type { ISettings } from 'src/settings';
 
-export function shouldConfigureGlobalMoment(app: App) {
+export const shouldConfigureGlobalMoment = (app: App): boolean => 
   // XXX: If the calendar plugin is installed, just use those settings.
   // Otherwise, we are responsible for configuring weekStart and localeOverride.
-  return !(<any>app).plugins.getPlugin('calendar')?._loaded;
-}
+   !(app as any).plugins.getPlugin('calendar')?._loaded
+;
 
-export function tryToConfigureGlobalMoment(app: App, settings: ISettings) {
+export const tryToConfigureGlobalMoment = (
+  app: App,
+  settings: ISettings,
+): void => {
   // XXX: If the calendar plugin is installed, just use those settings.
   // Otherwise, we are responsible for configuring weekStart and localeOverride.
   if (shouldConfigureGlobalMoment) {
     configureGlobalMomentLocale(settings.localeOverride, settings.weekStart);
   }
-}
+};
