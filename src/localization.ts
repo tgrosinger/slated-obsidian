e481d588765326d881d3a29d17e57272c277119a
Liam Cain <hi@liamca.in>
2021-02-01 12:15:38 -0500

    actually call shouldConfigureGlobalMoment func in localization

--- src/localization.ts
+++ src/localization.ts
@@ -2,11 +2,10 @@ import type { App } from 'obsidian';
 import { configureGlobalMomentLocale } from 'obsidian-calendar-ui';
 import type { ISettings } from 'src/settings';
 
-export const shouldConfigureGlobalMoment = (app: App): boolean => 
+export const shouldConfigureGlobalMoment = (app: App): boolean =>
   // XXX: If the calendar plugin is installed, just use those settings.
   // Otherwise, we are responsible for configuring weekStart and localeOverride.
-   !(app as any).plugins.getPlugin('calendar')?._loaded
-;
+  !(app as any).plugins.getPlugin('calendar')?._loaded;
 
 export const tryToConfigureGlobalMoment = (
   app: App,
@@ -14,7 +13,7 @@ export const tryToConfigureGlobalMoment = (
 ): void => {
   // XXX: If the calendar plugin is installed, just use those settings.
   // Otherwise, we are responsible for configuring weekStart and localeOverride.
-  if (shouldConfigureGlobalMoment) {
+  if (shouldConfigureGlobalMoment(app)) {
     configureGlobalMomentLocale(settings.localeOverride, settings.weekStart);
   }
 };
